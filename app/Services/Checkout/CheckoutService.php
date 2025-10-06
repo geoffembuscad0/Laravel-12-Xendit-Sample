e1fcce7bd7d7ed417192dc8042021f2eabf862cf
geoffembuscad0 <geoff.embuscado@yahoo.com.ph>
2025-10-06 20:08:37 +0800

    changes for the laravel 12 compatibility.

--- app/Services/Checkout/CheckoutService.php
+++ app/Services/Checkout/CheckoutService.php
@@ -1,8 +1,10 @@
 <?php
 namespace app\Http\Services\Checkout;
 
+use Xendit\Invoice\CreateInvoiceRequest;
 use Xendit\InvoiceClient; // or however the client is imported in new version
 use Xendit\Configuration;
+use Xendit\Invoice\InvoiceApi;
 
 class CheckoutService {
 
@@ -26,9 +28,18 @@ class CheckoutService {
         $defParams['success_redirect_url'] = $data['redirect_url'];
         $params = array_merge($defParams, $data);
         $response = [];
-
+        $invoiceInstance = new InvoiceApi();
+        $create_invoice_request = new CreateInvoiceRequest([
+            'external_id' => 'test1234',
+            'description' => 'Test Invoice',
+            'amount' => 10000,
+            'invoice_duration' => 172800,
+            'currency' => 'IDR',
+            'reminder_time' => 1
+        ]);
+        $for_user_id = "62efe4c33e45694d63f585f0";
         try {
-            $response = InvoiceXendit::createInvoice($params);
+            $result = $invoiceInstance->createInvoice($create_invoice_request, $for_user_id);
         } catch (\Throwable $e) {
             $response['message'] = $e->getMessage();
         }
